d33008fc157aec683ec99ed2139d0519c17745a9
jadonding <1520173329@mail.jxust.edu.cn>
2023-04-02 13:51:27 +0800

    fix(ScheduleJob): 定时任务执行失败问题修复

--- src/main/java/cn/jdblg/job/ScheduleJob.java
+++ src/main/java/cn/jdblg/job/ScheduleJob.java
@@ -2,19 +2,24 @@ package cn.jdblg.job;
 
 import cn.jdblg.service.PushService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 /**
  * @author jadonding
  */
+@Slf4j
 @Component
+@EnableScheduling
 @RequiredArgsConstructor
 public class ScheduleJob {
     private final PushService pushService;
 
     @Scheduled(cron = "${jdblg.cron:0 30 8 * * ?}")
     public void goodMorning() {
+        log.info("开始执行定时推送任务");
         pushService.push();
     }
 }
